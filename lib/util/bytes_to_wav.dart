055fcd70966e1c213b769d755e11e0a46a537441
Raj Nakarja <raj@siliconwitchery.com>
2024-04-22 13:32:58 +0200

    Image and audio parsing working

--- lib/util/bytes_to_wav.dart
+++ lib/util/bytes_to_wav.dart
@@ -1,13 +1,21 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
-import 'package:audioplayers/audioplayers.dart'; // Uncomment to listen to audio
 
 Uint8List _uint32to8(int value) =>
     Uint8List(4)..buffer.asUint32List()[0] = value;
 
 Uint8List _uint16to8(int value) =>
     Uint8List(2)..buffer.asUint16List()[0] = value;
+
+Uint8List offsetPcm(Uint8List inData) {
+  List<int> outData = [];
+  inData.forEach((element) {
+    outData.add((element + 128) % 256);
+  });
+  var outDataUint8 = Uint8List.fromList(outData);
+  return outDataUint8.buffer.asUint8List();
+}
 
 Uint8List bytesToWav(Uint8List pcmBytes, int bitDepth, int sampleRate) {
   print("converting ${pcmBytes.length} bytes to wav");
@@ -27,13 +35,7 @@ Uint8List bytesToWav(Uint8List pcmBytes, int bitDepth, int sampleRate) {
   output.add(_uint16to8(bitDepth));
   output.add(utf8.encode('data'));
   output.add(_uint32to8(pcmBytes.length));
-  output.add(pcmBytes);
-
-  print(output.toBytes());
-
-  // Uncomment to listen to audio
-  final player = AudioPlayer();
-  player.play(BytesSource(output.toBytes()));
+  output.add(offsetPcm(pcmBytes.buffer.asUint8List()));
 
   return output.toBytes();
 }
