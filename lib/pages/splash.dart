11d7249e462be21811269afd7c70f2a6625b8342
technoexponent <subhajitpal@technoexponent.com>
2024-05-10 12:30:53 +0530

    unused location classes deleted

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,33 +1,18 @@
 import 'dart:async';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:noa/locationService.dart';
 import 'package:noa/models/app_logic_model.dart' as app;
 import 'package:noa/pages/login.dart';
 import 'package:noa/pages/noa.dart';
 import 'package:noa/style.dart';
-import 'package:noa/util/location_state.dart';
 import 'package:noa/util/switch_page.dart';
 
 class SplashPage extends ConsumerWidget {
   const SplashPage({super.key});
-  Future<void> _getLocation() async {
-    try {
-      Position position = await LocationService().getCurrentLocation();
-      String? address =
-      await LocationService().getAddressFromCoordinates(position);
 
-      updateName(address?? "not Specified");
-
-      print(address);
-    } catch (e) {
-      print("Error fetching location: $e");
-    }
-  }
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    _getLocation();
+
     WidgetsBinding.instance.addPostFrameCallback((_) {
       ref.read(app.model).triggerEvent(app.Event.init);
       Timer(const Duration(milliseconds: 1500), () {
