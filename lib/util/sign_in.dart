82d045fe51cd09bf4167267d33bb81329c1a1448
Raj Nakarja <raj@siliconwitchery.com>
2024-05-11 12:43:20 +0200

    WIP google signin

--- lib/util/sign_in.dart
+++ lib/util/sign_in.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:logging/logging.dart';
 import 'package:noa/noa_api.dart';
@@ -44,12 +45,23 @@ class SignIn {
 
       await checkInternetConnection();
 
-      final GoogleSignInAccount? account = await GoogleSignIn(
-        clientId: Platform.isIOS
-            ? dotenv.env['GOOGLE_IOS_CLIENT_ID']
-            : dotenv.env['ANDROID_ANDROID_CLIENT_ID'],
-        scopes: ['email'],
-      ).signIn();
+      GoogleSignInAccount? account;
+
+      if (Platform.isAndroid) {
+        account = await GoogleSignIn(
+          serverClientId: kReleaseMode
+              ? dotenv.env['GOOGLE_ANDROID_CLIENT_ID']
+              : dotenv.env['GOOGLE_ANDROID_DEBUG_CLIENT_ID'],
+          scopes: ['email'],
+        ).signIn();
+      }
+
+      if (Platform.isIOS) {
+        account = await GoogleSignIn(
+          clientId: dotenv.env['GOOGLE_IOS_CLIENT_ID'],
+          scopes: ['email'],
+        ).signIn();
+      }
 
       final GoogleSignInAuthentication auth = await account!.authentication;
 
