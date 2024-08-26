75fd248f1a57ff1903d49c17d71118d91fda93c9
Abeer Vaishnav <abeer.vaishnav13@gmail.com>
2024-08-26 14:22:54 -0700

    Added another message for user to stay steady while the image is being taken

--- assets/lua_scripts/main.lua
+++ assets/lua_scripts/main.lua
@@ -99,9 +99,11 @@ while true do
         end)
 
         if state:has_been() > 0.2 and image_taken == false then
+            graphics:append_text("capturing photo... stay steady", "\u{F0002}")
+            frame.sleep(0.5)
             frame.camera.capture {}
             image_taken = true
-            graphics:append_text("photo captured!", "\u{F0002}")
+            graphics:append_text("photo captured!", "\u{F0007}")
             frame.sleep(0.5)
             graphics:append_text("tap to finish", "\u{F0010}")
         end
